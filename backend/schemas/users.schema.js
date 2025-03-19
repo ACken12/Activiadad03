1c6af5474ff404be90fe8bc860fa05722cc46b09
ACken <kendallangulo2@gmail.com>
2025-03-18 20:10:19 -0600

    I create a schema  for user

--- backend/schemas/users.schema.js
+++ backend/schemas/users.schema.js
@@ -1,11 +1,9 @@
 const Joi = require('joi');
 
-const id = Joi.string().uuid();
-const name = Joi.string().alphanum().min(3).max(15);
+const email = Joi.string().email().min(5).max(30);
 const password = Joi.number().integer().min(10);
 
-
 const createUserSchema = Joi.object({
-  name: name.required(),
-  price: password.required(),
+  password: password.required(),
+  email: email.required(),
 });
