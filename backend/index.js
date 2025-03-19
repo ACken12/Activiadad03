ff0ed30a5e4269d09d713ce33ef9e2e05d4ba690
ACken <kendallangulo2@gmail.com>
2025-03-18 22:17:43 -0600

    I fix the error

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require("cors");
-const bdPostgresql = require('./bd.js');
 const routes = require('./routes/routes.js');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
@@ -13,8 +12,6 @@ app.use(express.json());
 const port = 3000;
 const IP = "192.168.1.123";
 
-
-routes(app);
 
 app.use(logErrors);
 app.use(errorHandler);
