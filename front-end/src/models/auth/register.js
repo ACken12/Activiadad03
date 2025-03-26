02bae9f1e2a6500673ecf4a5cea7444cf97ceabc
ACken <kendallangulo2@gmail.com>
2025-03-25 19:59:58 -0600

    I add a validation for the register

---
+++ front-end/src/models/auth/register.js
@@ -0,0 +1,32 @@
+import signupValidation from '../../schemas/signup.schema.js';
+
+document.getElementById("signup-form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const data = {
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value
+    };
+
+    try {
+        const validation = signupValidation.validateForm(data);
+
+        if (!validation.isValid) {
+            // Show errors
+            Object.keys(validation.errors).forEach(field => {
+                const errorElement = document.getElementById(`${field}Error`);
+                if (errorElement) {
+                    errorElement.textContent = validation.errors[field];
+                    errorElement.style.display = 'block';
+                }
+            });
+            return;
+        }else {
+            const response = await register(data);
+            console.log("registro exitoso", response);
+            window.location.href = "../../front-end/html/login.html";
+        }
+    } catch (error) {
+        console.error("Error en el login:", error);
+    }
+});
---
+++ front-end/src/schemas/signup.schema.js
@@ -0,0 +1,43 @@
+const signupValidation = {
+    
+
+    validateEmail: (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!email || !emailRegex.test(email)) {
+            return 'Correo electrónico inválido';
+        }
+        return null;
+    },
+
+    validatePassword: (password) => {
+        if (!password || password.length < 6) {
+            return 'La contraseña debe tener al menos 6 caracteres';
+        }
+        if (!/(?=.*[A-Z])/.test(password)) {
+            return 'La contraseña debe contener al menos una mayúscula';
+        }
+        if (!/(?=.*\d)/.test(password)) {
+            return 'La contraseña debe contener al menos un número';
+        }
+        return null;
+    },
+
+    
+
+    validateForm: (formData) => {
+        const errors = {};
+        
+        const emailError = signupValidation.validateEmail(formData.email);
+        if (emailError) errors.email = emailError;
+
+        const passwordError = signupValidation.validatePassword(formData.password);
+        if (passwordError) errors.password = passwordError;
+
+        return {
+            isValid: Object.keys(errors).length === 0,
+            errors
+        };
+    }
+};
+
+export default signupValidation;
